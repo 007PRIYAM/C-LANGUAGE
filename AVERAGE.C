1bbe4b8b43522743d68da8dff93c5096a378cd0a
007PRIYAM <55493655+007PRIYAM@users.noreply.github.com>
2019-09-18 18:39:12 +0530

    Add files via upload

---
+++ AVERAGE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float  average,a,b,c;
+ printf("enter 3 numbers");
+ scanf("%d%d%d",&a,&b,&c);
+ average = (a+b+c)/3;
+ printf("%f",average);
+ getch();
+ }
+
